44fe6628a0168b6707080c82274f49296b8cb366
Andrew Nagi <andrew.nagi@gmail.com>
2010-12-13 15:28:19 +0000

    Add rvmrc

--- db/schema.rb
+++ db/schema.rb
@@ -9,21 +9,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100305164758) do
+ActiveRecord::Schema.define(:version => 20100305165202) do
 
   create_table "posts", :force => true do |t|
     t.string   "name"
     t.text     "content"
-    t.datetime "post_date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "username"
-    t.string   "email"
-    t.string   "password_hash"
-    t.string   "password_salt"
+    t.date     "post_date"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
