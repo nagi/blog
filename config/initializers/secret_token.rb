a0fc6b35c56147f92972299092946004405fed9c
nagi <andrew.nagi@gmail.com>
2011-06-04 16:22:00 +0100

    Overwrite app with new rails3 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '87135ec36279e7159058b7b4fb8c85d721689d96d5d30aba83f1e3d11d710a9d069cf3f060fd97004897ccaf3e7834bf97a1e1d3f80310e1bab33b051d16f677'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_blog_session',
-  :secret      => 'bf7d07fa85988c500888600ce67107ad93fc241fb7230072b450f3815eeadd7f78db80af47a6d167520b3faa781db205735cdc2789e02d05318e58d993830c27'
-}
+Blog::Application.config.session_store :cookie_store, :key => '_blog_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# Blog::Application.config.session_store :active_record_store
